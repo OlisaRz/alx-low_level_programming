4c72a31f6242131212b8939ad088b7a1ef4d2419
Rosemary Olisa Okonda <olisaokonda@gmail.com>
2022-06-02 13:56:29 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,15 @@
 #include <unistd.h>
 
 /**
- * main - Use to return the integer 1
+ * main - use to return the integer 1
  *
- * Return: Always 0 (Success)
+ * Description: use to print standard output in the main function
+ *
+ * Return: return the value 1
  */
 
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 return(1);
 }
