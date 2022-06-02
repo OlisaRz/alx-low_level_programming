96d7a3e8d210673bc137ae46b658ddcc6f6c9d52
Rosemary Olisa Okonda <olisaokonda@gmail.com>
2022-06-02 10:43:07 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - it prints a function
+ *
+ * Description: It prints a string
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcomeis a piece of art,\n");
+return (0);
+}
