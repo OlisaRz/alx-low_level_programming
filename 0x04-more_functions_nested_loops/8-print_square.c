29328de86437cf4a5283e2cdfecdfcd43adb3de1
Rosemary Olisa Okonda <olisaokonda@gmail.com>
2022-06-10 15:03:23 +0000

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,24 @@
-#include
+#include "main.h"
+
+/**
+ * print_square - prints squares
+ * @size: input integer
+ * Return: Always 0
+ */
+void print_square(int size)
+{
+	int m, n;
+
+	if (size > 0)
+	{
+		for (n = 0; n < size; n++)
+		{
+			for (n = 0; n < size; n++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
+
