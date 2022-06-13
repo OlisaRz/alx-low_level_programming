1d090716d030f127e4028dd7a55edd13f6e3eedb
Rosemary Olisa Okonda <olisaokonda@gmail.com>
2022-06-13 17:54:13 +0000

    function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+void rest_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+#endif
